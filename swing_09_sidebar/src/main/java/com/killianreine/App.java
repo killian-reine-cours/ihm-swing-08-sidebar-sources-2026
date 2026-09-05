00d7cdccc21d742df39e973a5fe77f774a39783a
killianreine <killianreine@gmail.com>
2026-09-05 22:34:58 +0200

    Initialisation du projet

---
+++ swing_09_sidebar/src/main/java/com/killianreine/App.java
@@ -0,0 +1,13 @@
+package com.killianreine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
